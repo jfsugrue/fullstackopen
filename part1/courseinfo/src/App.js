1b5a21e3d298942fe7829488548a8e92a77ee78e
James Sugrue <james@softwarex.co.nz>
2023-06-05 20:03:10 +1200

    ex 1.6
    exercise 1.6

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -44,11 +44,11 @@ const App = () => {
       {
         name: 'Fundamentals of React',
         exercises: 10
-      },
+      }, 
       {
         name: 'Using props to pass data',
         exercises: 7
-      },
+      }, 
       {
         name: 'State of a component',
         exercises: 14
